cd974a0d16223fc2373303f6c4f7985eb2b383a5
agogte <agogte@gmu.edu>
2023-02-12 17:12:54 -0500

    v1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import HeroImage from '../assets/IMG_8031.jpeg';
-import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
+// import {MdOutlineKeyboardArrowRight} from 'react-icons/md'
 
 const Home = () => {
   return (
@@ -11,16 +11,16 @@ const Home = () => {
                     I'm a Software Engineer
                 </h2>
                 <p className='py-4 text-gray-500'>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. At soluta unde sunt esse consequuntur quis nobis nulla in amet deleniti obcaecati cumque expedita officia nemo, voluptatum numquam temporibus eaque ipsam.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quia animi nesciunt laboriosam consequuntur impedit ratione deleniti ipsum incidunt. Ducimus?
                 </p>
                 <div>
-                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                        Portfolio<span className='group-hover:rotate-90 duration-300'> <MdOutlineKeyboardArrowRight size={25} className="ml-1"/></span> 
-                    </button>
+                    {/* <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                        Resume<span className='group-hover:rotate-90 duration-300'> <MdOutlineKeyboardArrowRight size={25} className="ml-1"/></span> 
+                    </button> */}
                 </div>
             </div>
             <div>
-                <img src={HeroImage} alt="pp" className='mx-auto md:w-full w-2/3 rounded-2xl' />
+                <img src={HeroImage} alt="pp" className='mx-auto md:w-2/3 w-2/3 rounded-2xl' />
             </div>
         </div>
     </div>
