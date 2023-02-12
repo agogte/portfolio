f95dc653af42ce2e6e3043431791a8b50fd1ee9b
agogte <agogte@gmu.edu>
2023-02-12 17:31:38 -0500

    final v1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
         <div className='max-w-screen-lg mx-auto flex flex-col  md:flex-row items-center px-4 justify-center h-full'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                    I'm a Software Engineer
+                    Hello!
                 </h2>
                 <p className='py-4 text-gray-500'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quia animi nesciunt laboriosam consequuntur impedit ratione deleniti ipsum incidunt. Ducimus?
+                    I'm a soon to be college grad looking for an entry level job as a Software/DevOps Engineer 👨🏻‍💻. I currently work as a Graduate Teaching Assistant at Computer Science department at George Mason University. I love travelling and spend most of my time in the great outdoors. 🧗🏻
                 </p>
                 <div>
                     {/* <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
