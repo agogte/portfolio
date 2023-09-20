c423ec357eb574a69d02e8df65429dd14f4a7773
agogte <agogte@gmu.edu>
2023-09-20 13:39:54 -0400

    added 2 new projects to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,8 +3,10 @@ import tictactoe from "../assets/portfolio/tictactoe.png"
 import connect4 from "../assets/portfolio/connect4.png"
 import password_generator from "../assets/portfolio/password_generator.png"
 import snake from "../assets/portfolio/snake.png"
-import myportfolio from "../assets/portfolio/portfolio.png"
+// import myportfolio from "../assets/portfolio/portfolio.png"
+import candycrush from "../assets/portfolio/candycrush.png"
 import hangman from "../assets/portfolio/hangman.png"
+import whowantstobeamillionair from "../assets/portfolio/whowantstobeamillionarie.png"
 
 const Portfolio = () => {
 
@@ -12,32 +14,44 @@ const Portfolio = () => {
         {
             id:1,
             src: tictactoe,
+            title: "Tic Tac Toe",
             href: 'https://agogte.github.io/tic-tac-toe/'
         },
         {
             id:2,
             src: connect4,
+            title: "Connect 4",
             href: 'https://agogte.github.io/connect4/'
         },
         {
             id:3,
             src: password_generator,
+            title: "Password generator",
             href: 'https://agogte.github.io/passgen_JS/'
         },
         {
             id:4,
             src: snake,
+            title: "Snake",
             href: 'https://agogte.github.io/snake/'
         },
         {
             id:5,
-            src: myportfolio,
-            href: 'https://agogte.github.io/portfolio/'
+            src: hangman,
+            title: "Hangman",
+            href: 'https://agogte.github.io/hangman/'
         },
         {
             id:6,
-            src: hangman,
-            href: 'https://agogte.github.io/hangman/'
+            src: whowantstobeamillionair,
+            title: "Who wants to be a millionaire?",
+            href: 'https://agogte.github.io/whowantstobeamillionaire/'
+        },
+        {
+            id:7,
+            src: candycrush,
+            title: "Candy Crush Clone",
+            href: 'https://agogte.github.io/candycrushclone/'
         }
     ]
 
@@ -54,11 +68,12 @@ const Portfolio = () => {
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
             {
-                portfolios.map(({id, src, href}) => (
+                portfolios.map(({id, src,title, href}) => (
                     <div key='id'className='shadow-md hover:scale-110 duration-500 py-2 rounded-lg shadow-white'>
                         <a href={href} target='_blank' rel='noopener noreferrer'>
                         <img src={src} alt="" className='w-20 mx-auto'/>
-                        <p>Go to Project</p>
+                        <p className='text-xl py-2'>{title}</p>
+                        <p className='text-sm'>Go to Project</p>
                         </a>
                     </div>
                 ))
